3579de485b6d563c4df80b14ce085f99a8def901
Matheus Fraga <devfraga@gmail.com>
2024-03-05 21:09:50 -0300

    AULA 01 Criando HTML, CSS

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      'sans': ['Poppins', 'sans-serif']
+    },
+    extend: {
+      backgroundImage:{
+        "home": "url('/assets/bg.png')"
+      }
+    },
+  },
+  plugins: [],
+}
+
