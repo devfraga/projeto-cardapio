92067a0dfb59c0fd08bd90093492d2ee040aeebb
Matheus Fraga <devfraga@gmail.com>
2024-03-06 22:32:13 -0300

    AULA 02 - Ajustando telefone

--- script.js
+++ script.js
@@ -185,7 +185,7 @@ checkoutBtn.addEventListener("click", function(){
   }).join("")
 
   const message = encodeURIComponent(cartItems)
-  const phone = "67996123728"
+  const phone = "NUMERO_DO_TELEFONE"
 
   window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
 
